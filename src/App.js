d1bf910d0b415f38ddca863bc01428fea2347585
shoubizheng <zheng_shoubi@hotmail.com>
2019-11-30 13:35:33 +0800

    using children properties

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         <p>This is working</p>
         <Person name='Mini' age = '29'/>
         <Person name = 'Maxi' age ='30'/>
-        <Person name ='Lili' age ='25'/>
+        <Person name ='Lili' age ='25'>My hobby: Swimming</Person>
       </div>
     );
     //return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Hi I\'m a react App!!!'))
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react' //jsx is tranformed to react createElement
 
 const person = (props)=>{
-return <p>Return jsx from Person! I'm {props.name}, {props.age} year old</p>
+return (
+        <div>
+            <p>Return from jsx! I'm {props.name}, {props.age}year old.</p>
+            <p>{props.children}</p>
+        </div>
+    )
 };
 
 export default person;
