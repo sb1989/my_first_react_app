a5b254d8332ad3d88b6b8a1ee5c532407a7d9d2a
shoubizheng <zheng_shoubi@hotmail.com>
2019-11-30 14:45:47 +0800

    bind onClick event handler to button

--- src/App.js
+++ src/App.js
@@ -10,12 +10,18 @@ class App extends Component {
       {name: 'Wawa', age: 26}
     ]
   }
+
+  switchNameHandler = () =>{
+    console.log('Was clicked');
+  }
+
   render() {
     return (
       //jsx
       <div className="App">
         <h1>Welcome to react</h1>
         <p>This is working</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
         <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}/>
         <Person name = {this.state.persons[2].name} age = {this.state.persons[2].agee}>My hobby: Swimming</Person>
