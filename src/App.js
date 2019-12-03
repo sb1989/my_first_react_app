1af6be96c8a2f7d18e76e89b964d12d8234a830d
shoubizheng <zheng_shoubi@hotmail.com>
2019-12-03 23:15:49 +0800

    dynamically hide content

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       {name: 'Yeye', age: 25},
       {name: 'Yaya', age: 29},
       {name: 'Wawa', age: 26}
-    ]
+    ],
+
+    showPerson: false
   }
 
   switchNameHandler = (newName) =>{
@@ -27,6 +29,10 @@ class App extends Component {
       {name: 'Wawa', age: 29}
     ]})
   }
+  togglePersonsHandler = ()=>{
+    const doesShow = this.state.showPerson;
+    this.setState({showPerson: !doesShow})
+  }
 
   render() {
     return (
@@ -34,13 +40,18 @@ class App extends Component {
       <div className="App">
         <h1>Welcome to react</h1>
         <p>This is working</p>
-        <button onClick={this.switchNameHandler.bind(this, 'Yiyi')}>Switch Name</button>
-        <Person 
-        click={this.switchNameHandler.bind(this, 'Yiyi')} 
-        changed={this.nameChangedHandler}
-        name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
-        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}/>
-        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}>My hobby: Swimming</Person>
+        {/*<button onClick={this.switchNameHandler.bind(this, 'Yiyi')}>Switch Name</button>*/}
+        <button onClick={this.togglePersonsHandler}>Toggle Person</button>
+       {
+        this.state.showPerson ? <div>
+            <Person 
+            click={this.switchNameHandler.bind(this, 'Yiyi')} 
+            changed={this.nameChangedHandler}
+            name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
+            <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}/>
+            <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}>My hobby: Swimming</Person>
+          </div> : null
+        } 
       </div>
     );
     //return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Hi I\'m a react App!!!'))
